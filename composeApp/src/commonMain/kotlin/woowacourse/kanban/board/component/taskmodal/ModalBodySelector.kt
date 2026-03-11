73e04aa579ec79e172a6f8cacfda554d754b17e7
byunghyunkim0 <bb05qudgus@gamil.com>
2026-03-11 16:38:12 +0900

    feat: 모달 옵션 그리드 구현

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/taskmodal/ModalBodySelector.kt
@@ -0,0 +1,65 @@
+package woowacourse.kanban.board.component.taskmodal
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ModalBodySelector(modifier: Modifier = Modifier, content: List<@Composable () -> Unit>) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(3),
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+    ) {
+        items(content) {
+            it()
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ModalBodySelectorOptionPreview() {
+    val names = listOf("In Progress", "To Do", "Done")
+    val contents = names.map {
+        @Composable {
+            ModalOptionButton(
+                modifier = Modifier, onClick = {},
+                content = {
+                    ModalOptionStatus(
+                        modifier = Modifier,
+                        text = it,
+                    )
+                },
+            )
+        }
+    }
+
+    ModalBodySelector(content = contents)
+}
+
+@Preview
+@Composable
+private fun ModalOptionAssigneePreview() {
+    val names = listOf("커비", "바드", "다이노", "아오", "하로")
+    val contents = names.map {
+        @Composable {
+            ModalOptionButton(
+                modifier = Modifier, onClick = {},
+                content = {
+                    ModalOptionAssignee(
+                        modifier = Modifier,
+                        name = it,
+                    )
+                },
+            )
+        }
+    }
+
+    ModalBodySelector(content = contents)
+}
